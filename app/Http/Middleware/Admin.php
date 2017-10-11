d9e5ac066940361ec4fdd5995726da2ae3e1c2fb
lunatravelint <lunatravelint@n1plcpnl0058.prod.ams1.secureserver.net>
2017-10-11 07:00:19 -0700

    website is working

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -19,7 +19,7 @@ class Admin
         if(Auth::check()){
 
 
-            if(Auth::user()->is_active === 1){
+            if(Auth::user()->is_active == 1){
 
                 return $next($request);
 
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>MyTrip Travel Agency</title>
+    <title>Luna travel</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Travel Company">
     <meta name="author" content="Hitesh Jariwla">
